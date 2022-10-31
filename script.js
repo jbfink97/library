3da9c3d7bae2493a5ec49afc70b6622cd22e2a61
Johnny <johnnyfink97@gmail.com>
2022-10-31 14:05:18 -0400

    set up basic html and css files. got the color/layout down, next is js.

---
+++ script.js
@@ -0,0 +1,23 @@
+let myLibarary = [];
+
+function Book(title, author, pages, readStatus){
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.readStatus = readStatus;
+}
+
+Book.prototype.info = function(){
+    let infoString;
+    if (this.readStatus.toLowerCase() == 'n' || this.readStatus.toLowerCase() == 'no'){
+        infoString = this.title + ' by ' + this.author + ', ' + this.pages + ' pages, not read yet';
+    } else {
+        infoString = this.title + ' by ' + this.author + ', ' + this.pages + ' pages, has been read';
+    }
+    return infoString;
+}
+
+function addBookToLibrary(){
+    //
+}
+
